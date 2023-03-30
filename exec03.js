e5a6fa13bfc32a31653ed36a79732ced30b845d9
Pedrin/Arroz <89743866+pedrosantos2@users.noreply.github.com>
2023-03-30 20:04:14 -0300

    Create exec03.js

---
+++ exec03.js
@@ -0,0 +1,57 @@
+//A)
+    
+    // for(i = 1; i < 20; i+=2 ){
+    //     i+
+    //     console.log(i)
+    // }
+    
+//B)
+    //   for(i = 2; i < 200; i*=2 ){
+    //     i+
+    //     console.log(i)
+    // }
+    
+//C)
+    //     
+    //     let result = 0;
+    //     for(let i = 0; i <= 110; i++){
+    //         if(i % 2 !== 0){
+    //     result += i;
+    //     }
+    // }
+    // console.log(result);
+    // 
+        
+//D)
+      
+    //  var sequencia = []
+
+    
+    // for (var n = 2; n <= 20; n+=2) {
+    //   var a = Math.pow(n, 2)
+    //   sequencia.push(a)
+    // }
+
+    // console.log(sequencia)
+    
+//E)
+    
+    // var sequencia = [1, 1]
+    
+    // for (var n = 2; n < 7; n++) {
+    //   var a = sequencia[n-1]
+    //   var b = sequencia[n-2]
+    //   var c = a + b
+    //   sequencia.push(c)
+    // }
+    
+    // console.log(sequencia)
+    
+//F)
+
+    // for(i = 0; i < 50; i++){
+    //     for(j = 2; j < i; j+=8){
+    //     j+=0
+    // }
+    //     console.log(j)
+    // }
